0b869cfd52aa2c4aa3e15348dd3aa57455cde0d2
iU24IT048 <gdemi0409@gmail.com>
2026-09-11 11:27:51 +0530

    Update firewall_config.sh

--- starter/firewall_config.sh
+++ starter/firewall_config.sh
@@ -4,22 +4,21 @@
 # Complete the commands below.
 
 # TODO 1: Open TCP port 8080
-
+sudo firewall-cmd --add-port=8080/tcp
 
 # TODO 2: Open TCP port 9000
-
+sudo firewall-cmd --add-port=9000/tcp
 
 # TODO 3: List the currently configured ports
-
+sudo firewall-cmd --list-ports
 
 # TODO 4: Remove TCP port 8080
-
+sudo firewall-cmd --remove-port=8080/tcp
 
 # TODO 5: Permanently open TCP port 3000
-
+sudo firewall-cmd --permanent --add-port=3000/tcp
 
 # TODO 6: Reload the firewall
-
+sudo firewall-cmd --reload
 
 exit 0
-
